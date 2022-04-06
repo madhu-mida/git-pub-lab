8a485b7b2d44551f35bff4ed9cde5904ab70a830
Madhumida <smadhumida@gmail.com>
2022-04-06 12:15:08 -0700

    setting data in models

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const drinks = require("./models/drinks.js")
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
+})
+
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
 })
 
 app.listen(PORT, () => {
