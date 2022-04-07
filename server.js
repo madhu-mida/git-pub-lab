fd5e11560eb464d28751affe1b98698788b34664
Madhumida <smadhumida@gmail.com>
2022-04-07 12:29:47 -0700

    Adding Foods and Styling

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001
 
 const drinks = require("./models/drinks.js")
+const foods = require("./models/food.js")
 
 
 app.get("/", (req, res) => {
@@ -17,7 +18,15 @@ app.get("/drinks", (req, res) => {
 
 app.get("/drinks/:id", (req, res) => {
     // res.send(drinks[req.params.id])
-    res.render("drinks_show.ejs", {drink: drinks[req.params.id]})
+    res.render("drinks_show.ejs", { drink: drinks[req.params.id] })
+})
+
+app.get("/foods", (req, res) => {
+    res.render("food_index.ejs", { allFoods: foods })
+})
+
+app.get("/foods/:id", (req, res) => {
+    res.render("food_show.ejs", { food: foods[req.params.id] })
 })
 
 app.listen(PORT, () => {
